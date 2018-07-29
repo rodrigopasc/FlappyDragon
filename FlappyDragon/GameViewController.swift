3542dc7b065418e783286eaecb7f62af1eee08c6
Rodrigo Paschoaletto <rodrigopaschoaletto@gmail.com>
2018-07-29 19:49:37 -0300

    Setting up GameScene and GameViewController.

--- FlappyDragon/GameViewController.swift
+++ FlappyDragon/GameViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
+var stage: SKView!
+
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        stage = view as! SKView
+        stage.ignoresSiblingOrder = true
+        
+        presentScene()
+    }
+    
+    func presentScene() {
+        let scene = GameScene(size: CGSize(width: 320, height: 568))
+        scene.scaleMode = .aspectFill
+        stage.presentScene(scene)
     }
 
     override var prefersStatusBarHidden: Bool {
